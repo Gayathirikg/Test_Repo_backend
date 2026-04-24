49f29fe788687f17fd8b2740c659edf6ac520dc6
Gayathiri K G <gayuvenkatgv@gmail.com>
2026-04-24 09:50:25 +0530

    fix: fallback mongo uri

--- config/db.js
+++ config/db.js
@@ -2,15 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-
-await mongoose.connect("mongodb://127.0.0.1:27017/expenses");
-
-    console.log("MongoDB connected");
-
+    const uri = process.env.MONGO_URI || "mongodb+srv://gayathirikgganesh_db_user:BNqSmdtkhAlJlQAF@cluster0.err2zn0.mongodb.net/expensedb?retryWrites=true&w=majority&appName=Cluster0";
+    await mongoose.connect(uri);
+    console.log("MongoDB connected ✅");
   } catch (error) {
-
-    console.log("MongoDB connection error", error);
-
+    console.log("MongoDB connection error----", error);
   }
 };
 
